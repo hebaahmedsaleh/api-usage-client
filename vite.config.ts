45715f9adbd34791338e5d7a8166eab7098256d0
hebaahmed400@gmail.com <hebaahmed400@gmail.com>
2025-11-06 20:54:53 +0200

    add build fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-   optimizeDeps: {
-    include: ["react-window"],
-  },
-  server: {
-    port: 4000,
-    proxy: {
-  "/api": {
-    target: "http://localhost:8000",
-    changeOrigin: true,
-    secure: false,
-  },
-    },
-  },
+  base: "./",
 });
